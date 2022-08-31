a7559ac7d6804781c88589c55da3b58b743d01ee
Nel <nel.renaudin@gmail.com>
2022-08-31 23:59:27 +0200

    Handle numbers and operators inputs by stocking and displaying them

--- script.js
+++ script.js
@@ -1,33 +1,99 @@
-function add(nb1, nb2) {
-  return Number(nb1) + Number(nb2);
-}
-function substract(nb1, nb2) {
-  return nb1 - nb2;
-}
-function multiply(nb1, nb2) {
-  return nb1 * nb2;
-}
-function divide(nb1, nb2) {
-  return (nb1 / nb2).toFixed(2);
-}
+const add = (nb1,nb2) => Number(nb1) + Number(nb2);
+const substract = (nb1,nb2) => nb1 - nb2;
+const multiply = (nb1,nb2) => nb1 * nb2;
+const divide = (nb1,nb2) => nb1 / nb2;
 
-function operate(nb1, nb2, operator) {
+const operation = {};
+
+function operate() {
   let result;
-  switch (operator) {
+  switch (operation.operator) {
     case 'add':
-      result = add(nb1, nb2);
+      result = add(operation.nb1, operation.nb2);
       break;
     case 'substract':
-      result = substract(nb1, nb2);
+      result = substract(operation.nb1, operation.nb2);
       break;
     case 'multiply':
-      result = multiply(nb1, nb2);
+      result = multiply(operation.nb1, operation.nb2);
       break;
     case 'divide':
-      result = divide(nb1, nb2);
+      result = divide(operation.nb1, operation.nb2);
       break;
     default:
       console.log('can\'t handle operator');
   }
   return result;
+}
+
+const numberButtons = document.querySelectorAll('#numbers>button');
+numberButtons.forEach(btn => btn.addEventListener('click', handleNbInput));
+const oprtButtons = document.querySelectorAll('#operators>button');
+oprtButtons.forEach(btn => btn.addEventListener('click', handleOpInput));
+
+function handleNbInput(event) {
+  let input = event.target.textContent;
+  if (!operation.operator) {
+    if (operation.nb1) {
+      operation.nb1 += input;
+    } else {
+      operation.nb1 = input;
+    }
+  } else {
+    if (operation.nb2) {
+      operation.nb2 += input;
+    } else {
+      operation.nb2 = input;
+    }
+  }
+  displayInput(input);
+}
+function handleOpInput(event) {
+  let input = event.target;
+  if (!operation.nb1) {
+    return;
+  }
+  if (!operation.nb2) {
+    if (operation.operator) {
+      display.textContent = display.textContent.slice(0, -1);
+    }
+    operation.operator = input.id;
+  } else {
+    let result = operate();
+    displayResult(result);
+    operation.nb1 = result;
+    operation.operator = input.id;
+    delete operation.nb2;
+  }
+  displayInput(input.textContent);
+}
+
+const operateButton = document.querySelector('button#operate');
+operateButton.addEventListener('click', () => {
+  if (!checkOperation()) return; //operation not valid
+  let result = operate();
+  displayResult(result);
+  operation.nb1 = result;
+  delete operation.operator;
+  delete operation.nb2;
+});
+
+function checkOperation() {
+  if (operation.nb1 && operation.nb2 && operation.operator) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const display = document.querySelector('#display');
+
+function displayInput(input) {
+  if (display.textContent === '0') {
+    display.textContent = '';
+  }
+  display.textContent += input;
+}
+function displayResult(result) {
+  display.textContent = result;
 }
