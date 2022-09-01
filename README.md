5435da920db56c1f913137fa53a3f05ca9836e8c
Nel <nel.renaudin@gmail.com>
2022-09-01 19:06:08 +0200

    Refactor code

--- README.md
+++ README.md
@@ -1,3 +1,57 @@
 # calculator
 
 A simple web calculator that performs operations on numbers.
+
+# algo
+
+function operations(nb1, nb2)
+ return result
+
+function operate(operation)
+ check if operation is valid, if not return
+ invoke correct operation function
+ save result as nb1
+ if nextOperator exist, put it as operator and delete nextOperator
+ if not delete operator
+ delete nb2
+ update display
+
+function handleNumberInput(input)
+ if there is not an operator yet
+  add input to nb1
+ if there is an operator
+  add input to nb2
+ update display
+
+function handOperatorInput(input)
+ if nb1 dont exist
+  return
+ if nb1 exist and nb2 don't exist
+  store input as operator
+ if nb2 exist
+  store input as nextOperator
+  operate
+ update display
+
+function updateDisplay(operation)
+ loop through every operation element
+ if element name is operator
+  add element value to display between space
+ add element value to display
+
+function goBack
+ if nb2 exist
+  if nb2 converted to string is longer than 1
+   remove last character of nb2
+  if nb2 converted to string is equal to 1
+   delete nb2
+ else if operator exist
+  delete operator
+ else if nb1 exist
+  if nb1 converted to string is longer than 1
+   remove last character of nb1
+  if nb1 converted to string is equal to 1
+   nb1 equal to 0
+ update display
+
+  
--- script.js
+++ script.js
@@ -1,226 +1,196 @@
-const add = (nb1,nb2) => Number(nb1) + Number(nb2);
+const add = (nb1,nb2) => nb1 + nb2;
 const substract = (nb1,nb2) => nb1 - nb2;
 const multiply = (nb1,nb2) => nb1 * nb2;
-const divide = (nb1,nb2) => {
-  if (nb2 == 0) {
-    clear();
-    return 0;
-    //showPop('What are you doing?');
-  } else {
-    return nb1 / nb2;
-  }
-};
+const divide = (nb1,nb2) => nb1 / nb2;
 
 const operation = {};
 
-function operate() {
-  let result;
-  switch (operation.operator) {
-    case 'add':
-      result = add(operation.nb1, operation.nb2);
-      break;
-    case 'substract':
-      result = substract(operation.nb1, operation.nb2);
-      break;
-    case 'multiply':
-      result = multiply(operation.nb1, operation.nb2);
-      break;
-    case 'divide':
-      result = divide(operation.nb1, operation.nb2);
-      result = Math.round((result + Number.EPSILON) * 100) / 100;
-      break;
-    default:
-      console.log('can\'t handle operator');
-  }
-  return result;
-}
-
-const numberButtons = document.querySelectorAll('#numbers>button');
-numberButtons.forEach(btn => btn.addEventListener('click', handleNbInput));
-const oprtButtons = document.querySelectorAll('#operators>button');
-oprtButtons.forEach(btn => btn.addEventListener('click', event => handleOpInput(event.target.textContent)));
-
-function handleNbInput(event) {
-  let input;
-  if ((event >= 0 && event < 10) || event === '.') {
-    input = event;
+function checkCurrent() {
+  if (operation.nb2) {
+    return 'nb2';
+  } else if (operation.operator) {
+    return 'operator';
   } else {
-    input = event.target.textContent;
+    return 'nb1';
   }
-
-  // Don't allow more than one dot
-  if (input === '.') {
-    if (operation[checkCurrentNb()].toString().includes('.')) {
-      return;
-    }
-  }
-  if (!operation.operator) {
-    if (operation.nb1) {
-      operation.nb1 += input;
-    } else {
-      operation.nb1 = input;
-    }
-  } else {
-    if (operation.nb2) {
-      operation.nb2 += input;
-    } else {
-      operation.nb2 = input;
-    }
-  }
-  displayInput(input);
-}
-function handleOpInput(op) {
-  if (!operation.nb1) {
-    return;
-  }
-  if (!operation.nb2) {
-    if (operation.operator) {
-      display.textContent = display.textContent.slice(0, -1);
-    }
-    operation.operator = convertOp(op);
-  } else {
-    let result = operate();
-    displayResult(result);
-    operation.nb1 = result;
-    operation.operator = input.id;
-    delete operation.nb2;
-  }
-  displayInput(' ' + op + ' ');
-}
-
-function convertOp(op) {
-  let opString;
-  switch (op) {
-    case '+':
-      opString = 'add';
-      break;
-    case '-':
-      opString = 'substract';
-      break;
-    case '*':
-    case 'x':
-      opString = 'multiply';
-      break;
-    case '/':
-    case '÷':
-      opString = 'divide';
-      break;
-  }
-  return opString;
-}
-
-function checkCurrentNb() {
-  return (operation.operator) ? 'nb2' : 'nb1'
 }
 
 const operateButton = document.querySelector('button#operate');
-operateButton.addEventListener('click', () => {
-  if (!checkOperation()) return; //operation not valid
-  let result = operate();
-  displayResult(result);
-  operation.nb1 = result;
-  delete operation.operator;
-  delete operation.nb2;
-});
+operateButton.addEventListener('click', operate);
 
-function checkOperation() {
-  if (operation.nb1 && operation.nb2 && operation.operator) {
-    return true;
+function operate() {
+  if (!checkIfOperationValid()) {
+    return;
+  }
+  let nb1 = Number(operation.nb1);
+  let nb2 = Number(operation.nb2);
+  let op = convertOpToString(operation.operator);
+  let result;
+  switch(op) {
+    case 'add':
+      result = add(nb1, nb2);
+      break;
+    case 'substract':
+      result = substract(nb1, nb2);
+      break;
+    case 'multiply':
+      result = multiply(nb1, nb2);
+      break;
+    case 'divide':
+      if (nb2 == '0') return;
+      result = divide(nb1, nb2);
+      break;
+  }
+  result = Math.round((result + Number.EPSILON) * 100) / 100;
+  operation.nb1 = result;
+  if (operation.nextOperator) {
+    operation.operator = operation.nextOperator;
+    delete operation.nextOperator;
   } else {
-    return false;
+    delete operation.operator;
+  }
+  delete operation.nb2;
+  updateDisplay();
+}
+
+function checkIfOperationValid() {
+  return (operation.nb1 && operation.nb2 && operation.operator) ? true : false;
+}
+
+function convertOpToString(op) {
+  switch (op) {
+    case '+':
+      return 'add';
+    case '-':
+      return 'substract';
+    case 'x':
+      return 'multiply';
+    case '÷':
+      return 'divide';
+  }
+}
+
+const numberButtons = document.querySelectorAll('#numbers>button:not(:last-child)');
+numberButtons.forEach(btn => btn.addEventListener('click', event => handleNumberInput(event.target.textContent)));
+
+function handleNumberInput(nb) {
+  if (!operation.operator) {
+    (operation.nb1) ? operation.nb1 += nb : (nb === '0') ? operation.nb1 = 0 : operation.nb1 = nb;
+  } else {
+    (operation.nb2) ? operation.nb2 += nb : operation.nb2 = nb;
+  }
+  updateDisplay();
+}
+const dotButton = document.querySelector('button#dot')
+dotButton.addEventListener('click', handleDotInput);
+
+function handleDotInput() {
+  if (operation[checkCurrent()] && operation[checkCurrent()].toString().includes('.')) {
+    return;
+  }
+  if (checkCurrent() === 'nb1') {
+    operation.nb1 ? operation.nb1 += '.' : operation.nb1 = '0.';
+  } else if (checkCurrent() === 'nb2' || checkCurrent() === 'operator') {
+    operation.nb2 ? operation.nb2 += '.' : operation.nb2 = '0.';
+  }
+  updateDisplay();
+}
+
+const operatorButtons = document.querySelectorAll('#operators>button');
+operatorButtons.forEach(btn => btn.addEventListener('click', event => handleOperatorInput(event.target.textContent)));
+
+function handleOperatorInput(op) {
+  if (!operation.nb1) {
+    return;
+  }
+  if (operation.nb1 && !operation.nb2) {
+    operation.operator = op;
+    updateDisplay();
+  }
+  if (operation.nb2) {
+    operation.nextOperator = op;
+    operate();
   }
 }
 
 const display = document.querySelector('#display');
 
-function displayInput(input) {
-  if (display.textContent === '0') {
-    display.textContent = '';
+function updateDisplay() {
+  display.textContent = '';
+  for (let key in operation) {
+    if (key === 'operator') {
+      display.textContent += ' ' + operation[key] + ' ';
+    } else {
+      display.textContent += operation[key];
+    }
   }
-  display.textContent += input;
+  if (display.textContent === '') {
+    display.textContent = '0';
+  }
 }
-function displayResult(result) {
-  display.textContent = result;
-}
-
-
 
 const clearButton = document.querySelector('button#clear');
 clearButton.addEventListener('click', clear);
+
 function clear() {
   for (let key in operation) {
     delete operation[key];
   }
-  display.textContent = '0';
+  updateDisplay();
 }
 
 const backspaceButton = document.querySelector('button#back');
 backspaceButton.addEventListener('click', goBack);
+
 function goBack() {
-  if (operation.nb2) {
-    operation.nb2 = operation.nb2.toString().slice(0, -1);
-    display.textContent = display.textContent.slice(0, -1);
-    if (operation.nb2 === '-') {
-      delete operation.nb2;
-      display.textContent = display.textContent.slice(0, -1);
-    }
-  } else if (operation.operator) {
+  let c = checkCurrent();
+  if (c === 'operator') {
     delete operation.operator;
-    display.textContent = display.textContent.slice(0, -3);
-  } else if (operation.nb1 && operation.nb1.length === 1) {
-    operation.nb1 = 0;
-    display.textContent = 0;
-  } else if (operation.nb1) {
-    operation.nb1 = operation.nb1.toString().slice(0, -1);
-    display.textContent = display.textContent.slice(0, -1);
-    if (operation.nb1 === '-') {
-      delete operation.nb1;
-      display.textContent = 0;
-    }
+  } else if (operation[c] && operation[c].toString().replace('-','').length === 1) {
+    delete operation[c];
+  } else if (operation[c] && operation[c].toString().replace('-','').length > 1) {
+    operation[c] = operation[c].toString().slice(0, -1);
   }
+  updateDisplay();
 }
 
 const posNegButton = document.querySelector('button#pos-neg');
 posNegButton.addEventListener('click', switchPositiveNegative);
+
 function switchPositiveNegative() {
-  if (operation.nb2) {
-    operation.nb2 = -(operation.nb2);
-    let disp = display.textContent.split(' ');
-    disp[2] = operation.nb2;
-    display.textContent = disp.join(' ');
-  } else if (operation.nb1 && operation.operator) {
-    operation.nb1 = -(operation.nb1);
-    let disp = display.textContent.split(' ');
-    disp[0] = operation.nb1;
-    display.textContent = disp.join(' ');
-  } else if (operation.nb1) {
-    operation.nb1 = -(operation.nb1);
-    display.textContent = operation.nb1;
+  if (checkCurrent() === 'nb1' && !operation.nb1) {
+    return;
+  } else if (checkCurrent() === 'nb1' || checkCurrent() === 'nb2') {
+    operation[checkCurrent()] = -(operation[checkCurrent()]);
   }
+  updateDisplay();
 }
 
 window.addEventListener('keydown', handleKeyboardInput);
+
 function handleKeyboardInput(event) {
   let key = event.key;
-  if ((key >= 0 && key < 10) || key === '.') {
-    handleNbInput(key);
-  } else if (key === 'Backspace') {
-    goBack()
-  } else if (key === '+' || key === '-' || key === 'x' || key === '*' || key === '/') {
-    handleOpInput(convertOpKey(key));
+  key = convertKbInput(key);
+  if (!isNaN(key) && key !== ' ') {
+    handleNumberInput(key);
+  } else if (key === '.') {
+    handleDotInput();
+  } else if ('+-x÷'.includes(key)) {
+    handleOperatorInput(key);
   } else if (key === 'Enter') {
-    if (!checkOperation()) return; //operation not valid
-    let result = operate();
-    displayResult(result);
-    operation.nb1 = result;
-    delete operation.operator;
-    delete operation.nb2;
+    operate();
+  } else if (key === 'Backspace') {
+    goBack();
   }
 }
-function convertOpKey(key) {
-  if (key === '*') {
-    return 'x';
-  } else {
-    return key;
+
+function convertKbInput(key) {
+  switch (key) {
+    case '*':
+      return 'x';
+    case '/':
+      return '÷';
+    default:
+      return key;
   }
 }
