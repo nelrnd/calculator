2c43440827bcc24939a9082f60ed7e4e947217a8
Nel <nel.renaudin@gmail.com>
2023-08-01 10:01:12 +0200

    Create operate function

--- script.js
+++ script.js
@@ -19,3 +19,18 @@ function multiply(number1, number2) {
 function divide(number1, number2) {
   return number1 / number2
 }
+
+function operate(number1, number2, operator) {
+  switch (operator) {
+    case '+':
+      return add(number1, number2);
+    case '-':
+      return substract(number1, number2);
+    case '×':
+      return multiply(number1, number2);
+    case '÷':
+      return divide(number1, number2);
+    default:
+      return 'incorrect operator'
+  }
+}
