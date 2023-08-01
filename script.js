47e97dbaac43a906a78ad715b8dbfefe016d50b8
Nel <nel.renaudin@gmail.com>
2023-08-01 09:57:05 +0200

    Create operation variables and add, substract, multiply and divide functions

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('hello')
+let number1;
+let number2;
+let operator;
+
+// Logic functions
+
+function add(number1, number2) {
+  return number1 + number2
+}
+
+function substract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2
+}
